1647516f1f9900d001305bf549e2ca270b07fb44
Heran76 <heran76@gmail.com>
2026-02-18 19:25:59 +0100

    variables

---
+++ fundamentos_python/entrada_salida_input_print/variables.py
@@ -0,0 +1,11 @@
+#variables
+#creamos una variable para almacenar un valor, el nombre de la variable debe ser descriptivo y no puede contener espacios ni caracteres especiales, excepto el guion bajo
+nombre = "Marcos"
+edad = 50
+altura = 1.75
+es_estudiante = True    
+#imprimimos el valor de las variables
+print(nombre)
+print(edad)
+print(altura)
+print(es_estudiante)        
