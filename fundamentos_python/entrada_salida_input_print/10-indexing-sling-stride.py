d0750a0993bf25b4caa7108c35582be48bc6fd73
Heran76 <heran76@gmail.com>
2026-02-23 21:31:36 +0100

    sling

---
+++ fundamentos_python/entrada_salida_input_print/10-indexing-sling-stride.py
@@ -0,0 +1,19 @@
+#indexing   
+texto = "Python"
+print(texto[0]) #P
+print(texto[1]) #y
+print(texto[2]) #t
+print(texto[3]) #h
+print(texto[4]) #o
+print(texto[5]) #n
+#slicing
+print(texto[0:3]) #Python
+print(texto[3:6]) #PytHon
+print(texto[:3]) #Python
+print(texto[3:]) #PytHon
+#stride
+print(texto[0:6:2]) #Pto 
+print(texto[1:6:2]) #yhn
+print(texto[::2]) #Pto
+print(texto[1::2]) #yhn     
+        
