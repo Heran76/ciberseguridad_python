caf49d1e7905cfa18502c1171cf24724f74d9826
Heran76 <heran76@gmail.com>
2026-02-23 20:37:49 +0100

    lista,tuplas,dicionarios

---
+++ fundamentos_python/entrada_salida_input_print/9-lista_tuplas_diccionarios.py
@@ -0,0 +1,13 @@
+#tuplasa que son las tuplas
+tupla = (1, 2, 3, 4, 5)
+print(tupla)
+print(type(tupla)) 
+#listas
+lista = [1, 2, 3, 4, 5]
+print(lista)
+print(type(lista))
+#diccionarios
+diccionario = {"nombre": "Juan", "edad": 30, "ciudad": "Madrid"}
+print(diccionario)
+print(type(diccionario))        
+    
