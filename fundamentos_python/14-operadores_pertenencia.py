d46b4bc8bd0798efa67f4cfcbd88258ded344c99
Heran76 <heran76@gmail.com>
2026-04-07 20:41:16 +0200

    Operadores de pertenencia

---
+++ fundamentos_python/14-operadores_pertenencia.py
@@ -0,0 +1,11 @@
+'''Operadores de Pertencia 
+operadores de pertenencia se utilizan para verificar si un elemento 
+está presente en una secuencia (como una lista, tupla, conjunto o diccionario) o no. 
+Los operadores de pertenencia son "in" y "not in". El operador "in" devuelve True si 
+el elemento está presente en la secuencia, mientras que el operador "not in" devuelve True si el elemento 
+no está presente en la secuencia.'''
+
+string_1 = "Hola Mundo"
+
+print(f"¿La letra 'o' está en el string '{string_1}'? {'o' in string_1}") #El operador "in" se utiliza para verificar si la letra 'o' está presente en el string "Hola Mundo". En este caso, como la letra 'o' sí está presente en el string, el resultado será True.
+print(f"¿La letra 'z' está en el string '{string_1}'?) {'z' in string_1}") #El operador "in" se utiliza para verificar si la letra 'z' está presente en el string "Hola Mundo". En este caso, como la letra 'z' no está presente en el string, el resultado será False.
